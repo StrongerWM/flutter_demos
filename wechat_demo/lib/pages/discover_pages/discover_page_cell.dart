28149ffce85c51f970d0d3103f36d7347f646bdb
StrongerWM <wood203@126.com>
2023-02-03 15:03:07 +0800

    mine page UI complete

--- wechat_demo/lib/pages/discover_pages/discover_page_cell.dart
+++ wechat_demo/lib/pages/discover_pages/discover_page_cell.dart
@@ -1,54 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:wechat_demo/pages/discover_child_page.dart';
+import 'package:wechat_demo/pages/discover_pages/discover_child_page.dart';
 
 class DiscoverPageCell extends StatefulWidget {
-
-  final String? imageName;
-  final String? title;
+  final String imageName;
+  final String title;
   final String? subTitle;
   final String? subImageName;
 
-  const DiscoverPageCell({Key? key, this.imageName, this.title, this.subTitle, this.subImageName}) : super(key: key);
+  const DiscoverPageCell(
+      {Key? key,
+      required this.imageName,
+      required this.title,
+      this.subTitle,
+      this.subImageName})
+      : super(key: key);
+
   @override
   State<DiscoverPageCell> createState() => _DiscoverPageCellState();
 }
 
 class _DiscoverPageCellState extends State<DiscoverPageCell> {
   Color _currentColor = Colors.white;
+  final Color _selectColor = const Color.fromRGBO(220, 220, 220, 1);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
+      onTap: () {
         debugPrint('onTap');
         Navigator.of(context).push(MaterialPageRoute(
             builder: (BuildContext context) => DiscoverChildPage(
-              title: widget.title,
-            )));
+                  title: widget.title,
+                )));
         // Navigator.of(context).push(
         //   MaterialPageRoute(builder: builder(MaterialPageRoute(builder: builder)))
         //
         // )
+        // Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context){
+        //   return DiscoverChildPage(title: widget.title,);
+        // }));
       },
-      onTapDown: (TapDownDetails details){
+      onTapDown: (TapDownDetails details) {
         debugPrint('onTapDown');
         setState(() {
-          _currentColor = Colors.grey;
+          _currentColor = _selectColor;
         });
       },
-      onTapUp: (TapUpDetails details){
+      onTapUp: (TapUpDetails details) {
         debugPrint('onTapUp');
         setState(() {
           _currentColor = Colors.white;
         });
       },
-      onTapCancel: (){
+      onTapCancel: () {
         debugPrint('onTapCancel');
         setState(() {
           _currentColor = Colors.white;
         });
       },
-
       child: Container(
         color: _currentColor,
         child: Row(
@@ -57,9 +66,12 @@ class _DiscoverPageCellState extends State<DiscoverPageCell> {
             //left
             Container(
               padding: const EdgeInsets.all(10),
-              child:Row(
+              child: Row(
                 children: [
-                  Image(image: AssetImage(widget.imageName!),width: 20,),
+                  Image(
+                    image: AssetImage(widget.imageName!),
+                    width: 20,
+                  ),
                   const SizedBox(width: 10),
                   Text(widget.title!),
                 ],
@@ -70,14 +82,24 @@ class _DiscoverPageCellState extends State<DiscoverPageCell> {
               padding: const EdgeInsets.all(10),
               child: Row(
                 children: [
-                  Text(widget.subTitle ?? "" , style: const TextStyle(color: Colors.grey),),
+                  Text(
+                    widget.subTitle ?? "",
+                    style: const TextStyle(color: Colors.grey),
+                  ),
                   widget.subTitle != null
-                      ? const Image(image: AssetImage("images/icon_badge.png"),width: 10,)
-                      : const SizedBox(width: 1,),
-                  const Image(image: AssetImage("images/icon_right.png"),width: 15,),
+                      ? const Image(
+                          image: AssetImage("images/icon_badge.png"),
+                          width: 10,
+                        )
+                      : const SizedBox(
+                          width: 1,
+                        ),
+                  const Image(
+                    image: AssetImage("images/icon_right.png"),
+                    width: 15,
+                  ),
                 ],
               ),
-
             ),
           ],
         ),
--- wechat_demo/lib/pages/mine_page.dart
+++ wechat_demo/lib/pages/mine_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wechat_demo/pages/discover_pages/discover_page_cell.dart';
 
 class MinePage extends StatefulWidget {
   const MinePage({Key? key}) : super(key: key);
@@ -10,13 +11,142 @@ class MinePage extends StatefulWidget {
 class _MinePageState extends State<MinePage> {
   @override
   Widget build(BuildContext context) {
+    final double leftMargin = MediaQuery.of(context).size.width - 35;
+
     return Container(
+      color: Colors.lightBlue,
       child: Scaffold(
-        appBar: AppBar(title: const Text('我的'),backgroundColor: Colors.blue,),
-        body: const Center(
-          child: Text('mine'),
-        ),
-      ),
+          body: Stack(
+        //默认主轴方向是从里到外
+        children: [
+          //top
+          Container(
+              color: const Color.fromRGBO(220, 220, 220, 1),
+              child: MediaQuery.removePadding(
+                context: context,
+                removeTop: true,
+                child: ListView(
+                  children: [
+                    //头部
+                    topViewWidget(),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    //cell
+                    const DiscoverPageCell(
+                      imageName: "images/icon_pay.png",
+                      title: '支付',
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    //收藏，相册icon_album.png，卡包icon_card_bag.png，
+                    // 表情icon_expression.png，设置icon_settings.png
+                    const DiscoverPageCell(
+                      imageName: "images/icon_collection.png",
+                      title: '收藏',
+                    ),
+                    Container(
+                      color: Colors.white,
+                      height: 0.5,
+                      margin: EdgeInsets.only(right: leftMargin),
+                    ),
+                    const DiscoverPageCell(
+                      imageName: 'images/icon_card_bag.png',
+                      title: '卡包',
+                    ),
+                    Container(
+                      color: Colors.white,
+                      height: 0.5,
+                      margin: EdgeInsets.only(right: leftMargin),
+                    ),
+                    const DiscoverPageCell(
+                      imageName: 'images/icon_expression.png',
+                      title: '表情',
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const DiscoverPageCell(
+                      imageName: 'images/icon_settings.png',
+                      title: '设置',
+                    ),
+                  ],
+                ),
+              )),
+          //相机
+          Container(
+              margin: const EdgeInsets.only(top: 50, right: 10),
+              color: const Color.fromRGBO(0, 0, 0, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: const [
+                  Image(
+                    image: AssetImage("images/icon_camera.png"),
+                    height: 35,
+                  ),
+                ],
+              )),
+        ],
+      )),
     );
+  }
+
+  topViewWidget() {
+    return Container(
+        color: Colors.white,
+        height: 200,
+        child: Container(
+          //内容视图
+          // color: Colors.green,
+          margin: const EdgeInsets.only(top: 100, left: 10, right: 10),
+          child: Row(
+            children: [
+              //头像
+              Container(
+                height: 70,
+                width: 70,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(image: AssetImage('images/Avatar'
+                      '.png')),
+                  borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(left: 10,top: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //名称
+                      const SizedBox(
+                        height: 35,
+                        child: Text(
+                          'Lucy',
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      //文字+箭头
+                      SizedBox(
+                        height: 45,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: const [
+                            Text('something',style: TextStyle(fontSize: 15),),
+                            Image(image: AssetImage("images/icon_right.png"),
+                              fit: BoxFit.cover,width: 15,),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
